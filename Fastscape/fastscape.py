56bea7eb40d923a0a7e708bbfd22382f624a2ac9
Jesse Hahm <wjhahm@berkeley.edu>
2016-02-02 11:50:46 -0800

    quiver plot working, fixed receiver index bug!

--- Fastscape/fastscape.py
+++ Fastscape/fastscape.py
@@ -10,16 +10,17 @@ EPS 117, UC Berkeley, Spring 2016
 ###  Import required libraries
 import numpy as np
 import matplotlib.pyplot as plt
-
+"""
 ####################################
 #USER DEFINED LANDSCAPE VARIABLES
-"""
+
 # Scale of the grid; km
 xl = 10**1
 yl = 10**1
 #Resolution of the grid
 nx = 10**1
 ny = 10**1 #number of nodes
+nn = nx*ny
 
 # Create array that stores elevation scalar
 # Convention wherein single number describes node
@@ -33,13 +34,16 @@ xl = 10.
 yl = 5.
 nx = 10
 ny = 5
+nn = nx*ny
 h = np.array([9,0,0,0,6,6,6,5,4,3,
               2,2,2,2,5,5,5,4,4,2,
               3,3,3,3,5,4,3,2,1,0,
               2,2,2,2,5,5,5,4,4,2,
               0,0,0,0,6,6,6,5,4,3])
-
 #################
+
+
+
 dt = 1000 #yrs; timestep
 nstep = 1000 #number of timesteps
 n = 1 #Slope exponent
@@ -49,15 +53,14 @@ m = 0.4 # drainage area exponent
 # DERIVED VARIABLES
 dx = xl/(nx)
 dy = yl/(ny) 
-nn = nx*ny
 indexVector = np.arange(nn)
 reshaped_index = indexVector.reshape(ny,nx)
 def plot_mesh(oneD_in):
     """Take 1-d array, plot as color mesh grid"""
     grid = np.reshape(oneD_in,(ny,nx))
     plt.pcolormesh(grid)
-    plt.gca().invert_yaxis()
-    plt.show()
+    #plt.gca().invert_yaxis()
+    #plt.show()
 
 reshaped_h = h.reshape(ny,nx)
 
@@ -65,24 +68,23 @@ reshaped_h = h.reshape(ny,nx)
 diag_dist = np.sqrt((dx**2) + (dy**2))
 receiver = np.arange(nn)
 slope = np.zeros(nn)
-direction = np.zeros(nn)
+direction = np.full(nn, 4)
 twoD_index = indexVector.reshape(ny,nx)
 twoD_noBoundary = twoD_index[1:-1,1:-1]
 oneD_noBoundary = twoD_noBoundary.ravel()
 # to get boundaries; not this!
 
+dist_neighbors = np.array([diag_dist, dy, diag_dist,
+                           dx,        1,  dx,
+                           diag_dist, dy, diag_dist])
 #Build receiver array
 for ij in oneD_noBoundary:
     # if not on boundary:
     ij_neighbors =np.array([ij-nx-1, ij-nx, ij-nx+1,
                             ij-1,    ij,    ij+1,
-                            ij+nx+1, ij+nx, ij+nx+1])  
+                            ij+nx-1, ij+nx, ij+nx+1])  
     
     h_neighbors = h[ij_neighbors]
-
-    dist_neighbors = np.array([diag_dist, dy, diag_dist,
-                               dx,        1,  dx,
-                               diag_dist, dy, diag_dist])
 
     delta_h = h[ij] - h_neighbors    
     
@@ -119,41 +121,44 @@ print 'receiver'
 print reshaped_receiver
 print 'ndon'
 print reshaped_ndon
-plot_mesh(h)
 
 U = np.zeros(nn)
 V = np.zeros(nn)
 for ij in indexVector:
-    if ij == 0:
+    if direction[ij] == 0:
         U[ij] = -1
         V[ij] = 1
-    if ij == 1:
+    if direction[ij] == 1:
         U[ij] = 0
         V[ij] = 1
-    if ij == 2:
+    if direction[ij] == 2:
         U[ij] = 1
         V[ij] = 1
-    if ij == 3:
+    if direction[ij] == 3:
         U[ij] = -1
         V[ij] = 0
-    if ij == 4:
+    if direction[ij] == 4:
         U[ij] = 0
         V[ij] = 0
-    if ij == 5:
+    if direction[ij] == 5:
         U[ij] = 1
         V[ij] = 0
-    if ij == 6:
+    if direction[ij] == 6:
         U[ij] = -1
         V[ij] = -1
-    if ij == 7:
+    if direction[ij] == 7:
         U[ij] = 0
         V[ij] = -1
-    if ij == 8:
+    if direction[ij] == 8:
         U[ij] = 1
         V[ij] = -1
-        
+
+plot_mesh(h)
 qx = np.arange(nx)
 qy = np.arange(ny)
 qU = U.reshape(ny,nx)
 qV = V.reshape(ny,nx)
-Q = plt.quiver(qx,qy,qU,qV)
+Q = plt.quiver(qx+(dx/2.0),qy+(dy/2.0),qU,qV)
+
+plt.gca().invert_yaxis()
+plt.show()
