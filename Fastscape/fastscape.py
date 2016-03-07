93b22427985dcf9425a41ab70d94ec7e4d6cb184
jessehahm <wjhahm@berkeley.edu>
2016-03-07 14:06:04 -0800

    Time-loop working - creating eroded landscapes!

--- Fastscape/fastscape.py
+++ Fastscape/fastscape.py
@@ -15,7 +15,6 @@ import time
 start = time.time()
 
 
-"""
 ####################################
 #USER DEFINED LANDSCAPE VARIABLES
 
@@ -26,6 +25,7 @@ yl = 10**2
 nx = 10**2
 ny = 10**2 #number of nodes
 nn = nx*ny
+printFreq = 10
 
 # Create array that stores elevation scalar
 # Convention wherein single number describes node
@@ -42,16 +42,17 @@ ny = 5
 nn = nx*ny
 h = np.array([9,0,0,0,6,6,6,5,4,3,
               2,2,2,2,5,5,5,4,4,2,
-              3,3,3,3,5,4,3,2,1,0.5,
+              3,3,3,3,5,4,3,2,1,0,
               2,2,2,2,5,5,5,4,4,2,
               0,0,0,0,6,6,6,5,4,3], dtype=float)
 #################
+"""
 
 
-k = 2.0*10**(-4)
+k = 1.0*10**(-4)
 U = 2.0*10**(-3) #(m/yr)
-delta_t = 10.0**12 #yrs; timestep
-num_timesteps = 1000 #number of timesteps
+delta_t = 10.0**3 #yrs; timestep
+num_timesteps = 100 #number of timesteps
 n = 1.0 #Slope exponent
 m = 0.4 # drainage area exponent
 ################
@@ -65,7 +66,7 @@ reshaped_index = indexVector.reshape(ny,nx)
 def plot_mesh(oneD_in):
     """Take 1-d array, plot as color mesh grid"""
     grid = np.reshape(oneD_in,(ny,nx))
-    plt.pcolormesh(grid, cmap='RdBu')
+    plt.pcolormesh(grid, cmap='gist_earth')
     #plt.gca().invert_yaxis()
     #plt.show()
 
@@ -96,116 +97,111 @@ dist_neighbors = np.array([diag_dist, dy, diag_dist,
 # 2: Build donor array
 
 #Loop over timesteps
-#for t in range(num_timesteps):
+for istep in range(num_timesteps):    
     
-
-#Build receiver array
-#receiver array stores each node's lowest neighbor
-for ij in oneD_noBoundary:
-    # if not on boundary:
-    ij_neighbors =np.array([ij-nx-1, ij-nx, ij-nx+1,
-                            ij-1,    ij,    ij+1,
-                            ij+nx-1, ij+nx, ij+nx+1])  
+    #Build receiver array
+    #receiver array stores each node's lowest neighbor
+    for ij in oneD_noBoundary:
+        # if not on boundary:
+        ij_neighbors =np.array([ij-nx-1, ij-nx, ij-nx+1,
+                                ij-1,    ij,    ij+1,
+                                ij+nx-1, ij+nx, ij+nx+1])  
+        
+        h_neighbors = h[ij_neighbors]
     
-    h_neighbors = h[ij_neighbors]
-
-    delta_h = h[ij] - h_neighbors    
+        delta_h = h[ij] - h_neighbors    
+        
+        slope_neighbors = delta_h/dist_neighbors
     
-    slope_neighbors = delta_h/dist_neighbors
-
-    steepest_descent = max(slope_neighbors)
-    steepest_descent_index = np.argmax(slope_neighbors)
-    receiver[ij] = ij_neighbors[steepest_descent_index]
-    slope[ij] = steepest_descent
-    direction[ij] = steepest_descent_index    
-    delta_x[ij] = dist_neighbors[steepest_descent_index]
-reshaped_receiver = receiver.reshape(ny,nx)
-
-
-## ndon = total number of donors to a node
-ndon = np.zeros(nn,int)
-# donors = indices of donors to a node 
-donor = np.full([8,nn],-1,int)
-
-#Build donor and ndon array
-#donor array: list of nodes that drain to you
-for ij in indexVector:
-    if receiver[ij] != ij:   #if not myself
-        recij = receiver[ij]
-        donor[ndon[recij], recij] = ij
-        #Increment number of donors by one for this receiver
-        ndon[recij] = ndon[recij] + 1 
-
-reshaped_ndon = ndon.reshape(ny,nx)
-
-print 'h'
-print reshaped_h
-print 'index'
-print reshaped_index
-print 'receiver'
-print reshaped_receiver
-print 'ndon'
-print reshaped_ndon
-
-
-
-#%% ####### MAKE THE STACK
-
-# START WITH BASE-LEVELS / PITS 
-# (Where receiver[ij] = ij)
-
-baseLevels = receiver[receiver == indexVector]
-stack = np.empty(nn,int)
-
-#Create array of length nn, 'catchment'
-#where value = name of catchment
-#which is actually the baselevel!
-catchment = np.empty(nn,int)
-nstack = 0
-
-def add_to_stack(ij, cc):
-    global catchment 
-    global nstack
-    stack[nstack] = ij
-    catchment[ij] = cc    
-    nstack = nstack+1
-    for k in donor[0:ndon[ij],ij]:
-              add_to_stack(k, cc)
-
-for ij in baseLevels:
-    cc = ij
-    add_to_stack(ij, cc)
+        steepest_descent = max(slope_neighbors)
+        steepest_descent_index = np.argmax(slope_neighbors)
+        receiver[ij] = ij_neighbors[steepest_descent_index]
+        slope[ij] = steepest_descent
+        direction[ij] = steepest_descent_index    
+        delta_x[ij] = dist_neighbors[steepest_descent_index]
+    reshaped_receiver = receiver.reshape(ny,nx)
     
+    
+    ## ndon = total number of donors to a node
+    ndon = np.zeros(nn,int)
+    # donors = indices of donors to a node 
+    donor = np.full([8,nn],-1,int)
+    
+    #Build donor and ndon array
+    #donor array: list of nodes that drain to you
+    for ij in indexVector:
+        if receiver[ij] != ij:   #if not myself
+            recij = receiver[ij]
+            donor[ndon[recij], recij] = ij
+            #Increment number of donors by one for this receiver
+            ndon[recij] = ndon[recij] + 1 
+    
+    reshaped_ndon = ndon.reshape(ny,nx)
+    
+   
+    
+    #%% ####### MAKE THE STACK
+    
+    # START WITH BASE-LEVELS / PITS 
+    # (Where receiver[ij] = ij)
+    
+    baseLevels = receiver[receiver == indexVector]
+    stack = np.empty(nn,int)
+    
+    #Create array of length nn, 'catchment'
+    #where value = name of catchment
+    #which is actually the baselevel!
+    catchment = np.empty(nn,int)
+    nstack = 0
+    
+    def add_to_stack(ij, cc):
+        global catchment 
+        global nstack
+        stack[nstack] = ij
+        catchment[ij] = cc    
+        nstack = nstack+1
+        for k in donor[0:ndon[ij],ij]:
+                  add_to_stack(k, cc)
+    
+    for ij in baseLevels:
+        cc = ij
+        add_to_stack(ij, cc)
+        
+    
+    #%% Drainage area
+    area = np.ones(nn)*dx*dy
+    reversed_stack = stack[::-1]
+    
+    for ij in reversed_stack:
+        if receiver[ij] != ij:
+            area[receiver[ij]] = area[receiver[ij]] + area[ij] 
+    
+ #   print 'area:'
+#    print area.reshape(ny,nx)    
+    
+    #%% Calculate new heights
+    
+    #Two exceptional cases
+    # Boundaries: Don't add uplift, don't change h
+    # Local minima: Just add uplift
+    
+    #Add uplift to all non-boundary nodes
+    #for ij in oneD_noBoundary:
+      #  h[ij] = h[ij] + U*delta_t
+    
+    #Add uplift to all nodes away from boundary
+    for ij in oneD_noBoundary:
+        h[ij] = h[ij] + U*delta_t
+    #calculate erosion for all in stack
+    for ij in stack:
+        if (receiver[ij] != ij):
+         #   print ij
+            C = k*area[ij]*delta_t/delta_x[ij]
+            h[ij] = (h[ij] + C*h[receiver[ij]])/(1 + C) 
 
-#%% Drainage area
-area = np.ones(nn)*dx*dy
-reversed_stack = stack[::-1]
-
-for ij in reversed_stack:
-    if receiver[ij] != ij:
-        area[receiver[ij]] = area[receiver[ij]] + area[ij] 
-
-print 'area:'
-print area.reshape(ny,nx)    
-
-#%% Calculate new heights
-
-#Two exceptional cases
-# Boundaries: Don't add uplift, don't change h
-# Local minima: Just add uplift
-
-#Add uplift to all non-boundary nodes
-#for ij in oneD_noBoundary:
-  #  h[ij] = h[ij] + U*delta_t
-
-#Add height to all eroding nodes
-for ij in stack:
-    if (receiver[ij] != ij):
-        print ij
-        C = k*area[ij]*delta_t/delta_x[ij]
-        h[ij] = (h[ij] + C*h[receiver[ij]])/(1 + C) 
-#        h[ij] = (h[ij] + U*delta_t + C*h[receiver[ij]])/(1 + C) 
-
+    if (istep % 10 == 0):
+        plot_mesh(h)
+        plt.show()
 #%% Plotting
 print 'It took', time.time()-start, 'seconds.'
 
